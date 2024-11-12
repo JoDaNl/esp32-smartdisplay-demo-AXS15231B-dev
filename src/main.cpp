d161d0b85b793c573be52120811be1f69c9ded7a
JoDaNl <josdautzenberg@hotmail.com>
2024-11-12 09:53:26 +0100

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h>
+
+#include <esp32_smartdisplay.h>
+#include <ui/ui.h>
+
+void OnAddOneClicked(lv_event_t *e)
+{
+    static uint8_t cnt = 0;
+    cnt++;
+    lv_label_set_text_fmt(ui_lblCountValue, "%d", cnt);
+}
+
+void OnRotateClicked(lv_event_t *e)
+{
+    auto disp = lv_disp_get_default();
+    auto rotation = (lv_disp_rot_t)((lv_disp_get_rotation(disp) + 1) % (LV_DISP_ROT_270 + 1));
+    lv_disp_set_rotation(disp, rotation);
+}
+
+void setup()
+{
+#ifdef ARDUINO_USB_CDC_ON_BOOT
+    delay(2000);
+#endif
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    log_i("Board: %s", BOARD_NAME);
+    log_i("CPU: %s rev%d, CPU Freq: %d Mhz, %d core(s)", ESP.getChipModel(), ESP.getChipRevision(), getCpuFrequencyMhz(), ESP.getChipCores());
+    log_i("Free heap: %d bytes", ESP.getFreeHeap());
+    log_i("Free PSRAM: %d bytes", ESP.getPsramSize());
+    log_i("SDK version: %s", ESP.getSdkVersion());
+
+    smartdisplay_init();
+    log_i("JOS - smartdisplay_init done");
+
+    __attribute__((unused)) auto disp = lv_disp_get_default();
+    // lv_disp_set_rotation(disp, LV_DISP_ROT_90);
+    // lv_disp_set_rotation(disp, LV_DISP_ROT_180);
+    // lv_disp_set_rotation(disp, LV_DISP_ROT_270);
+
+    ui_init();
+    lv_timer_handler();
+}
+
+ulong next_millis;
+
+void loop()
+{
+    auto const now = millis();
+    if (now > next_millis)
+    {
+        next_millis = now + 500;
+
+        char text_buffer[32];
+        sprintf(text_buffer, "%lu", now);
+        lv_label_set_text(ui_lblMillisecondsValue, text_buffer);
+
+#ifdef BOARD_HAS_RGB_LED
+        auto const rgb = (now / 2000) % 8;
+        smartdisplay_led_set_rgb(rgb & 0x01, rgb & 0x02, rgb & 0x04);
+#endif
+
+#ifdef BOARD_HAS_CDS
+        auto cdr = analogReadMilliVolts(CDS);
+        sprintf(text_buffer, "%d", cdr);
+        lv_label_set_text(ui_lblCdrValue, text_buffer);
+#endif
+    }
+
+    lv_timer_handler();
+}
